92f49a323e610d0b53659b76345d4607545ce2b7
theelous3 <theegrandmaster@gmail.com>
2025-11-21 19:17:52 +0000

    complete rework of project

--- README.md
+++ README.md
@@ -1,37 +1,42 @@
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/F2F21OVNKF)
+
 # Isometric Box Generator for Aseprite
 This script lets you create boxes of various sizes in [Aseprite](https://www.aseprite.org/):
 
-![](./screenshots/screenshot-1.gif)
+![](https://i.imgur.com/QwJnhXM.mp4)
 
 You can set a custom color for each of the sides, set a highlight and stroke color. You can also choose between two types of the corners: 2px and 3px. You can read more about the differences [here](http://www.gas13.ru/v3/tutorials/isometric_pixelart_tutorial_the_two_types.php).
 
-There are two versions of the script: Regular and Lite. Lite version generates a stroke only boxes, intended for quick prototyping with basic wired box without any colors. 
 
-![](./screenshots/screenshot-3.gif)
+## Features:
+    + Customize X, Y and Z size of the box
+    + Customize colors for each side of the box
+    + Custom Stroke and Highlight colors
+    + Two types of the box: 3px and 2px corner
 
-Using Regular version you can set a custom color for each of the sides of the box. By default it will generate shades of your current foreground color. 
-
-![](./screenshots/screenshot-2.png)
-
-## Features
-- Custom X, Y and Z size of the box
-- Custom colors for Stroke, Highlight and each side of the box
-- Two types of the corners: 2px and 3px (Read more)
-- Optional Lite version of the script with a basic wired box for quick prototyping
+## New features:
+    + Wireframe mode for outlining
+    + Custom layer naming and auto increment
+    + Improved size setting ui
+    + Auto centering boxes
+    + Auto update of color palette with fg
 
 ## Requirements:
-- [Aseprite 1.2.10-beta2+](https://www.aseprite.org/beta) (“beta - Beta version - Preview features” branch in Steam)
+- >= [Aseprite 1.2.10-beta2+](https://www.aseprite.org/)
 - Color Mode: RGBA
 
 ## Installation:
 1. Open Aseprite
 2. Go to `File → Scripts → Open Scripts Folder`
-3. Place downloaded LUA files into previously opened directory
-4. Restart Aseprite
-You will find installed scripts under `File → Scripts` menu. You can add shortcuts to each script under `Edit → Keyboard Shortcuts` ([Read more](https://twitter.com/ThKasparrr/status/1059168255015825409))
+3. Place downloaded LUA file into folder
+4. Reload scripts (menu or press F5)
 
-You can also create a separate folder for these scripts for better organization ([Read more](https://twitter.com/TackRadGames/status/1059176888554397696))
+You will find installed scripts under `File → Scripts` menu. You can add shortcuts to each script under `Edit → Keyboard Shortcuts`
+
 
 ---
-Special thanks to [@davidcapello](https://twitter.com/davidcapello) for giving tips on how to improve the code 🙌
+
+## About this project
+
+This is a major and near complete overhaul fork of https://github.com/darkwark/isobox-for-aseprite
 
---
+++ [KAM&TL3] Isometric Box v1.1.0.lua
@@ -0,0 +1,548 @@
+--[[
+
+__/\\\\\\\\\\\_____/\\\\\\\\\\\_________/\\\\\_______/\\\\____________/\\\\__/\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\____/\\\\\\\\\______/\\\\\\\\\\\________/\\\\\\\\\_________
+__\/////\\\///____/\\\/////////\\\_____/\\\///\\\____\/\\\\\\________/\\\\\\_\/\\\///////////__\///////\\\/////___/\\\///////\\\___\/////\\\///______/\\\////////_________
+_______\/\\\______\//\\\______\///____/\\\/__\///\\\__\/\\\//\\\____/\\\//\\\_\/\\\___________________\/\\\_______\/\\\_____\/\\\_______\/\\\_______/\\\/_________________
+________\/\\\_______\////\\\__________/\\\______\//\\\_\/\\\\///\\\/\\\/_\/\\\_\/\\\\\\\\\\\___________\/\\\_______\/\\\\\\\\\\\/________\/\\\______/\\\__________________
+_________\/\\\__________\////\\\______\/\\\_______\/\\\_\/\\\__\///\\\/___\/\\\_\/\\\///////____________\/\\\_______\/\\\//////\\\________\/\\\_____\/\\\_________________
+__________\/\\\_____________\////\\\___\//\\\______/\\\__\/\\\____\///_____\/\\\_\/\\\___________________\/\\\_______\/\\\____\//\\\_______\/\\\_____\//\\\_______________
+___________\/\\\______/\\\______\//\\\___\///\\\__/\\\____\/\\\_____________\/\\\_\/\\\___________________\/\\\_______\/\\\_____\//\\\______\/\\\______\///\\\____________
+_________/\\\\\\\\\\\_\///\\\\\\\\\\\/______\///\\\\\/_____\/\\\_____________\/\\\_\/\\\\\\\\\\\\\\\_______\/\\\_______\/\\\______\//\\\__/\\\\\\\\\\\____\////\\\\\\\\\__
+_________\///////////____\///////////__________\/////_______\///______________\///__\///////////////________\///________\///________\///__\///////////________\/////////__
+__/\\\\\\\\\\\\\_________/\\\\\_______/\\\_______/\\\________________/\\\____________/\\\___________/\\\\\\\______________________________________________________________
+__\/\\\/////////\\\_____/\\\///\\\____\///\\\___/\\\/_____________/\\\\\\\________/\\\\\\\_________/\\\/////\\\___________________________________________________________
+___\/\\\_______\/\\\___/\\\/__\///\\\____\///\\\\\\/______________\/////\\\_______\/////\\\________/\\\____\//\\\_________________________________________________________
+____\/\\\\\\\\\\\\\\___/\\\______\//\\\_____\//\\\\____________________\/\\\___________\/\\\_______\/\\\_____\/\\\________________________________________________________
+_____\/\\\/////////\\\_\/\\\_______\/\\\______\/\\\\____________________\/\\\___________\/\\\_______\/\\\_____\/\\\_______________________________________________________
+______\/\\\_______\/\\\_\//\\\______/\\\_______/\\\\\\___________________\/\\\___________\/\\\_______\/\\\_____\/\\\______________________________________________________
+_______\/\\\_______\/\\\__\///\\\__/\\\_______/\\\////\\\_________________\/\\\___________\/\\\_______\//\\\____/\\\______________________________________________________
+________\/\\\\\\\\\\\\\/_____\///\\\\\/______/\\\/___\///\\\_______________\/\\\__/\\\_____\/\\\__/\\\__\///\\\\\\\/______________________________________________________
+_________\/////////////_________\/////_______\///_______\///________________\///__\///______\///__\///_____\///////_______________________________________________________
+
+  ISOMETRIC BOX GENERATOR 1.1.0 for Aseprite (https://aseprite.org)
+
+  This is the original project page, but I believe it to be abandoned.
+  https://darkwark.itch.io/isobox-for-aseprite
+
+    Overhaul by Mark Jameson @theelous3
+    https://theelous3.net
+    I frankly don't give a shite about the (lack of) pre existing licence & status
+    as there has been no movement for 6 years. Whatever power I have
+    for the addition of my own work, I release under the nov 21st 2025 version
+    of the MIT licence. I believe the changes in this file are so broad as to be
+    essentially entirely transformational. However, out of respect for the previous
+    dev I am going to include their details and notes etc.
+
+    Original by Kamil Khadeyev (@darkwark)
+    Twitter: http://twitter.com/darkwark
+    Dribbble: http://dribbble.com/darkwark
+    Website: http://darkwark.com
+
+    (c) 2018, November
+    All rights reserved or something
+
+    Features:
+        + Customize X, Y and Z size of the box
+        + Customize colors for each side of the box
+        + Custom Stroke and Highlight colors
+        + Two types of the box: 3px and 2px corner
+
+    New features:
+        + Wireframe mode for outlining
+        + Custom layer naming and auto increment
+        + Improved ui
+        + Auto centering tall boxes
+        + Auto update of color palette with fg
+
+    Requirements:
+        + Aseprite >= 1.2.10-beta2
+        + Color Mode: RGBA
+
+    Installation:
+        + Open Aseprite
+        + Go to `File → Scripts → Open Scripts Folder`
+        + Place downloaded LUA script into opened directory
+        + F5 to reload scripts, or select reload scripts from scripts menu
+
+    Usage:
+        + Go to `File → Scripts → [KAM&TL3] Isometric Box v1.1.0` to run the script
+        + You can also setup a custom hotkey under `Edit → Keyboard Shortcuts`
+
+]]
+
+
+---------------------------------------
+-- USER DEFAULTS --
+---------------------------------------
+local c = app.fgColor
+
+-- Default colors:
+local colors = {
+    stroke    = nil,
+    top       = nil,
+    left      = nil,
+    right     = nil,
+    highlight = nil
+}
+
+local function computeColorsFromFg(fg)
+    if not fg then return end
+
+    local h = fg.hsvHue
+    local s = fg.hsvSaturation
+    local v = fg.hsvValue
+
+    colors.stroke    = Color{ h=0, s=0, v=0, a=255 }
+    colors.top       = fg
+    colors.left      = Color{ h=h, s=s+0.3, v=v-0.1, a=255 }
+    colors.right     = Color{ h=h, s=s+0.3, v=v-0.4, a=255 }
+    colors.highlight = Color{ h=h, s=s-0.2, v=v+0.2, a=255 }
+end
+
+-- initial computation at script load
+computeColorsFromFg(app.fgColor)
+
+-- Use 3px corner by default:
+local use3pxCorner = false
+
+-- Default Max Sizes:
+local maxSize = {
+    x = math.floor(app.activeSprite.width),
+    y = math.floor(app.activeSprite.width),
+    z = math.floor(app.activeSprite.height)
+}
+
+-- Initial size defaults (one source of truth)
+local defaultSize = {
+    x = 5,
+    y = 5,
+    z = 10
+}
+
+
+
+---------------------------------------
+-- Colors Utility --
+---------------------------------------
+local function colorAsPixel(color)
+    return app.pixelColor.rgba(color.red, color.green, color.blue, color.alpha)
+end
+
+local function isColorEqual(a, b)
+    local pc = app.pixelColor
+
+    return pc.rgbaR(a) == pc.rgbaR(b) and
+           pc.rgbaG(a) == pc.rgbaG(b) and
+           pc.rgbaB(a) == pc.rgbaB(b) and
+           pc.rgbaA(a) == pc.rgbaA(b)
+end
+
+local function isColorEqualAt(x, y, color)
+    local pc = app.pixelColor
+    local pickedColor = app.activeImage:getPixel(x, y)
+
+    return isColorEqual(pickedColor, color)
+end
+
+---------------------------------------
+-- Flood Fill --
+---------------------------------------
+local function floodFill(x, y, targetColor, replacementColor)
+    if isColorEqual(targetColor, replacementColor) then return end
+    if not isColorEqualAt(x, y, targetColor) then return end
+
+    app.activeImage:putPixel(x, y, replacementColor)
+
+    floodFill(x+1, y, targetColor, replacementColor)
+    floodFill(x-1, y, targetColor, replacementColor)
+    floodFill(x, y+1, targetColor, replacementColor)
+    floodFill(x, y-1, targetColor, replacementColor)
+end
+
+---------------------------------------
+-- BASIC LINES --
+---------------------------------------
+local function hLine(color, x, y, len)
+    for i = 1, len do
+        app.activeImage:putPixel(x+i, y, color)
+    end
+end
+
+local function vLine(color, x, y, len)
+    for i = 1, len do
+        app.activeImage:putPixel(x, y+i, color)
+    end
+end
+
+---------------------------------------
+-- ISOMETRIC LINES --
+---------------------------------------
+local function isoLineDownRight(color, x, y, len)
+    for i=0,len do
+        local x1 = i*2
+        local x2 = x1+1
+        app.activeImage:putPixel(x+x1, y+i, color)
+        app.activeImage:putPixel(x+x2, y+i, color)
+    end
+end
+
+local function isoLineDownLeft(color, x, y, len)
+    for i=0,len do
+        local x1 = i*2
+        local x2 = x1+1
+        app.activeImage:putPixel(x-x1, y+i, color)
+        app.activeImage:putPixel(x-x2, y+i, color)
+    end
+end
+
+local function isoLineUpRight(color, x, y, len)
+    for i=0,len do
+        local x1 = i*2
+        local x2 = x1+1
+        app.activeImage:putPixel(x+x1, y-i, color)
+        app.activeImage:putPixel(x+x2, y-i, color)
+    end
+end
+
+local function isoLineUpLeft(color, x, y, len)
+    for i=0,len do
+        local x1 = i*2
+        local x2 = x1+1
+        app.activeImage:putPixel(x-x1, y-i, color)
+        app.activeImage:putPixel(x-x2, y-i, color)
+    end
+end
+
+
+---------------------------------------
+-- Cube position --
+---------------------------------------
+local function getBoxOrigin(xSize, ySize, zSize)
+    local spr     = app.activeSprite
+    local centerX = math.floor(spr.width  / 2)
+    local centerY = math.floor(spr.height / 2)
+
+    -- Full vertical span is roughly (xSize + ySize + zSize).
+    -- so the box's vertical midpoint is:
+    --     centerY + (zSize - xSize - ySize) / 2
+    --
+    -- We want that midpoint to line up with sprite centerY.
+    -- So we shift the "centerY" we pass into the drawing code by:
+    --     offset = (xSize + ySize - zSize) / 2
+    --
+    local offsetY = math.floor((xSize + ySize - zSize) / 2)
+    local baseY   = centerY + offsetY
+
+    return centerX, baseY
+end
+
+
+local function drawCube(type_, xSize, ySize, zSize, color)
+    local centerX, centerY = getBoxOrigin(xSize, ySize, zSize)
+
+    local a = (type_ == 1) and 0 or 1
+    local b = (type_ == 1) and 1 or 0
+
+    -- top plane
+    isoLineUpRight(color, centerX-a,         centerY,       xSize) -- bottom right
+    isoLineUpLeft (color, centerX,           centerY,       ySize) -- bottom left
+    isoLineUpLeft (color, centerX+xSize*2+b, centerY-xSize, ySize) -- top right
+    isoLineUpRight(color, centerX-ySize*2-1, centerY-ySize, xSize) -- top left
+
+    -- bottom plane
+    isoLineUpRight(color, centerX-a,         centerY+zSize, xSize) -- right
+    isoLineUpLeft (color, centerX,           centerY+zSize, ySize) -- left
+
+    -- verticals
+    vLine(color, centerX-a,         centerY,       zSize) -- middle
+    vLine(color, centerX-ySize*2-1, centerY-ySize, zSize) -- left
+    vLine(color, centerX+xSize*2+b, centerY-xSize, zSize) -- right
+end
+
+
+------------ Adding Colors: ------------
+
+local function fillCubeSides(xSize, ySize, zSize, topColor, leftColor, rightColor)
+    local centerX, centerY = getBoxOrigin(xSize, ySize, zSize)
+
+    local TRANSPARENT_COLOR = app.pixelColor.rgba(0, 0, 0, 0)
+
+    floodFill(centerX,   centerY-1, TRANSPARENT_COLOR, colorAsPixel(topColor))
+    floodFill(centerX-2, centerY+1, TRANSPARENT_COLOR, colorAsPixel(leftColor))
+    floodFill(centerX+1, centerY+1, TRANSPARENT_COLOR, colorAsPixel(rightColor))
+end
+
+
+local function addHighlight(type_, xSize, ySize, zSize, color)
+    local centerX, centerY = getBoxOrigin(xSize, ySize, zSize)
+
+    local alt = (type_ == 1) and 0 or 1
+
+    isoLineUpRight(color, centerX-alt, centerY,       xSize-1)
+    isoLineUpLeft (color, centerX,     centerY,       ySize-1)
+    vLine         (color, centerX-alt, centerY,       zSize-1)
+
+    app.activeImage:putPixel(centerX-alt, centerY, app.pixelColor.rgba(255, 255, 255, 255))
+end
+
+---------------------------------------
+-- LAYER MANAGEMENT --
+---------------------------------------
+local function newLayer(name)
+    local s   = app.activeSprite
+    local lyr = s:newLayer()
+    lyr.name  = name
+    s:newCel(lyr, 1)
+    return lyr
+end
+
+---------------------------------------
+-- USER INTERFACE / STATE --
+---------------------------------------
+local updating = false
+local labelIncrement = 0
+local lastLabelName  = ""
+
+local dlg = Dialog("[KAM&TL3] Isometric Box v1.1.0")
+
+
+local function updateDialogColorsFromFg(fg)
+    -- refresh our colour set
+    computeColorsFromFg(fg)
+
+    -- if the dialog is still around, push the values into the widgets
+    if dlg then
+        dlg:modify{ id="topColor",       color = colors.top }
+        dlg:modify{ id="leftColor",      color = colors.left }
+        dlg:modify{ id="rightColor",     color = colors.right }
+        dlg:modify{ id="highlightColor", color = colors.highlight }
+    end
+end
+
+local function onFgColorChange(ev)
+    local fg = ev and ev.color or app.fgColor
+    if not fg then return end
+    updateDialogColorsFromFg(fg)
+end
+
+app.events:on("fgcolorchange", onFgColorChange)
+
+dlg
+    :separator{ text="Size:" }
+    ---------------------------------------
+    -- Y (Left) --
+    ---------------------------------------
+    :slider{
+        id="ySize",
+        label="Left:",
+        min=1,
+        max=maxSize.y,
+        value=defaultSize.y,
+        onchange=function()
+            if updating then return end
+            updating = true
+
+            local v = dlg.data.ySize or defaultSize.y
+            v = math.max(1, math.min(maxSize.y, v))
+
+            dlg:modify{ id="ySize",    value=v }
+            dlg:modify{ id="ySizeNum", text=tostring(v) }
+
+            updating = false
+        end
+    }
+    :number{
+        id="ySizeNum",
+        label="->",
+        text=tostring(defaultSize.y),
+        onchange=function()
+            if updating then return end
+            updating = true
+
+            local v = tonumber(dlg.data.ySizeNum) or defaultSize.y
+            v = math.max(1, math.min(maxSize.y, v))
+
+            dlg:modify{ id="ySizeNum", text=tostring(v) }
+            dlg:modify{ id="ySize",    value=v }
+
+            updating = false
+        end
+    }
+
+    ---------------------------------------
+    -- X (Right) --
+    ---------------------------------------
+    :slider{
+        id="xSize",
+        label="Right:",
+        min=1,
+        max=maxSize.x,
+        value=defaultSize.x,
+        onchange=function()
+            if updating then return end
+            updating = true
+
+            local v = dlg.data.xSize or defaultSize.x
+            v = math.max(1, math.min(maxSize.x, v))
+
+            dlg:modify{ id="xSize",    value=v }
+            dlg:modify{ id="xSizeNum", text=tostring(v) }
+
+            updating = false
+        end
+    }
+    :number{
+        id="xSizeNum",
+        label="->",
+        text=tostring(defaultSize.x),
+        onchange=function()
+            if updating then return end
+            updating = true
+
+            local v = tonumber(dlg.data.xSizeNum) or defaultSize.x
+            v = math.max(1, math.min(maxSize.x, v))
+
+            dlg:modify{ id="xSizeNum", text=tostring(v) }
+            dlg:modify{ id="xSize",    value=v }
+
+            updating = false
+        end
+    }
+
+
+    ---------------------------------------
+    -- Z (Height) --
+    ---------------------------------------
+    :slider{
+        id="zSize",
+        label="Height:",
+        min=3,
+        max=maxSize.z,
+        value=defaultSize.z,
+        onchange=function()
+            if updating then return end
+            updating = true
+
+            local v = dlg.data.zSize or defaultSize.z
+            v = math.max(3, math.min(maxSize.z, v))
+
+            dlg:modify{ id="zSize",    value=v }
+            dlg:modify{ id="zSizeNum", text=tostring(v) }
+
+            updating = false
+        end
+    }
+    :number{
+        id="zSizeNum",
+        label="->",
+        text=tostring(defaultSize.z),
+        onchange=function()
+            if updating then return end
+            updating = true
+
+            local v = tonumber(dlg.data.zSizeNum) or defaultSize.z
+            v = math.max(3, math.min(maxSize.z, v))
+
+            dlg:modify{ id="zSizeNum", text=tostring(v) }
+            dlg:modify{ id="zSize",    value=v }
+
+            updating = false
+        end
+    }
+
+
+    :separator{ text="Colors:" }
+    :color {id="color",          label="Stroke:",    color = colors.stroke}
+    :color {id="topColor",       label="Top:",       color = colors.top}
+    :color {id="leftColor",      label="Left:",      color = colors.left}
+    :color {id="rightColor",     label="Right:",     color = colors.right}
+    :color {id="highlightColor", label="Highlight:", color = colors.highlight}
+    :check{
+        id="noFillAll",
+        label="No fill (black wireframe)",
+        selected=false
+    }
+
+    :separator()
+    :radio {id="typeOne", label="Corner:", text="3 px", selected=use3pxCorner}
+    :radio {id="typeTwo", text="2 px", selected=not use3pxCorner}
+
+    :separator()
+    :entry{
+        id="customLayerName",
+        label="Custom layer name:",
+        text=""
+    }
+
+:button{
+    id="ok",
+    text="Add Box",
+    onclick = function()
+        local data = dlg.data
+
+        app.transaction(function()
+            -----------------------------------------------------
+            -- sizes
+            -----------------------------------------------------
+            local xSize = data.xSize or defaultSize.x
+            local ySize = data.ySize or defaultSize.y
+            local zSize = data.zSize or defaultSize.z
+
+            -----------------------------------------------------
+            -- cube type --
+            -----------------------------------------------------
+            local cubeType = data.typeOne and 1 or 2
+
+            -----------------------------------------------------
+            -- layer naming --
+            -----------------------------------------------------
+            local customLayerName = data.customLayerName or ""
+            local layerName
+
+            if customLayerName ~= "" then
+                if customLayerName ~= lastLabelName then
+                    lastLabelName  = customLayerName
+                    labelIncrement = 0
+                    layerName      = customLayerName
+                else
+                    labelIncrement = labelIncrement + 1
+                    layerName      = customLayerName .. " " .. tostring(labelIncrement)
+                end
+            else
+                lastLabelName  = ""
+                labelIncrement = 0
+                layerName      = "Cube("..xSize.." "..ySize.." "..zSize..")"
+            end
+
+            -----------------------------------------------------
+            -- colors / wireframe --
+            -----------------------------------------------------
+            local wireframeOnly  = (data.noFillAll == true)
+
+            local strokeColor    = data.color
+            local topColor       = data.topColor
+            local leftColor      = data.leftColor
+            local rightColor     = data.rightColor
+            local highlightColor = data.highlightColor
+
+            if wireframeOnly then
+                strokeColor = Color{ r=0, g=0, b=0, a=255 }
+            end
+
+            -----------------------------------------------------
+            -- draw --
+            -----------------------------------------------------
+            newLayer(layerName)
+            drawCube(cubeType, xSize, ySize, zSize, strokeColor)
+
+            if not wireframeOnly then
+                fillCubeSides(xSize, ySize, zSize, topColor, leftColor, rightColor)
+                addHighlight(cubeType, xSize, ySize, zSize, highlightColor)
+            end
+        end)
+
+        app.command.Undo()
+        app.command.Redo()
+    end
+}
+    :show{wait=false}
